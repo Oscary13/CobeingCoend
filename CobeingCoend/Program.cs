3a8d762567dafe870010b1636d23d7fd3cec29db
Oscary13 <oscary_1320114020@uptecamac.edu.mx>
2022-11-11 09:38:15 -0600

    0

---
+++ CobeingCoend/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CobeingCoend
+{
+    internal class Program
+    {
+        public static long j = 10;
+        static void Main(string[] args)
+        {
+            Thread h1 = new Thread(hilo1);
+            Thread h2 = new Thread(hilo2);
+
+            h2.Start();
+            h1.Start();
+
+            Console.WriteLine(j);
+            Console.ReadLine();
+        }
+        public static void hilo1()
+        {
+            Console.WriteLine(j);
+        }
+
+        public static void hilo2()
+        {
+            j = 100000000;
+        }
+    }
+}
